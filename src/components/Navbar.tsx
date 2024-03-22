6b8eff07de91ff62be790b5db76f9c3ca5a0694c
shakilkhan80 <shakilkhan81198@gmail.com>
2024-03-22 20:12:05 +0600

    navbar added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { AppBar, Fab, styled } from "@mui/material";
+import { useParams } from "next/navigation";
+
 const NavList = [
   {
     name: "Home",
@@ -20,19 +25,35 @@ const NavList = [
     link: "/contact",
   },
 ];
+
+const StyledFab = styled(Fab)({
+  position: "absolute",
+  zIndex: 1,
+  top: -30,
+  left: 0,
+  right: 0,
+  margin: "0 auto",
+});
+
 const Navbar = () => {
+  const pathName= useParams()
+  console.log(pathName)
   return (
-    <div className="flex justify-between items-center">
-      <div>Shakil Khan</div>
-      <div className="hover:underline grid grid-cols-4 gap-2 items-center justify-center">
-        {NavList.map((item, index) => {
-          return (
-            <div key={index}>
-              <a href={item.link}>{item.name}</a>
-            </div>
-          );
-        })}
-      </div>
+    <div>
+      <AppBar sx={{ bgcolor: "#222" , color: "#c49b66" }} position="fixed">
+        <div className="flex font-bold  justify-between items-center mx-32 py-2">
+          <div className="text-4xl text-white">Shakil</div>
+          <div className="grid grid-cols-5 gap-2 items-center justify-center ">
+            {NavList.map((item, index) => {
+              return (
+                <div key={index}>
+                  <a href={item.link}>{item.name}</a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </AppBar>
     </div>
   );
 };
