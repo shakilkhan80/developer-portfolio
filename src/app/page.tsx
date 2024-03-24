0f5110da82ed0d9e17aabe68f27129532e3d3a55
shakilkhan80 <shakilkhan81198@gmail.com>
2024-03-24 09:45:44 +0600

    animate banner text added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,58 @@
 "use client";
 
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
+import { useState } from "react";
 import { Bounce, Fade } from "react-awesome-reveal";
+import { TypeAnimation } from "react-type-animation";
+
+const START_BOUNCE_TIME = 2000;
 
 export default function Home() {
+  const [doBounce, setDoBounce] = useState(true);
+
+  const startBounce = () => {
+    setTimeout(() => {
+      setDoBounce(false);
+    }, START_BOUNCE_TIME);
+    setTimeout(() => {
+      setDoBounce(true);
+    }, START_BOUNCE_TIME + 1000);
+  };
+
   return (
     <main className="text-white">
-     
-      <Bounce triggerOnce>
+      <section>
+        <div>
+          <Image src="/shakil.jpg" alt="Shakil Khan" width={200} height={200} />
+        </div>
+        <div className="text-center">
+          <h1 className="text-7xl font-medium">Welcome!</h1>
+          <div className="text-center leading-normal md:text-xl lg:text-5xl my-7">
+            I am &#160;
+            <TypeAnimation
+              speed={40}
+              deletionSpeed={70}
+              sequence={["Shakil Khan", 2000, "a Web Developer", 2000]}
+              style={{ textShadow: "1px 1px black", color: "#c49b66" }}
+              repeat={Infinity}
+            />
+          </div>
+          <AnimatePresence>
+            {doBounce && (
+              <motion.div
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -500 }}
+                transition={{ duration: 3, type: "spring", mass: 2 }}
+              >
+                <p> I a Web Developer</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      </section>
+
+      <Bounce cascade damping={0.5}>
         <p>I will animate only the first time you see me</p>
       </Bounce>
       <Fade cascade>
